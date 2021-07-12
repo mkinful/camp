fe50a6bb6e3d44a3a99e35e31f8ffe85a177e978
Your Name <epyon85@gmail.com>
2021-07-12 00:05:57 -0400

    updated links

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@
               }
             });
           });
+
+$(function (){
+  $('#reserveButton').click(function(){
+    $('#reserveModal').modal('show');
+  })
+})
+
+$(function(){
+  $('#loginButton').click(function(){
+    $('#loginModal').modal('show');
+  })
+})
